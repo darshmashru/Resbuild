bacee9446070a685a65fa5aba4c34038672429cc
Darsh Mashru <mashrudarsh31@gmail.com>
2023-11-06 13:08:00 +0530

    New Vercel Integration for Django

---
+++ vercel_app/settings.py
@@ -0,0 +1,7 @@
+INSTALLED_APPS = [
+    # ...
+    'anidya',
+]
+
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+WSGI_APPLICATION = 'vercel_app.wsgi.app'
---
+++ vercel_app/wsgi.py
@@ -0,0 +1,1 @@
+app = get_wsgi_application()
